8ec16f4e3757bef1f4f0a22420a6fe93984bfadd
Rishi Singhal <167010491+Rishi-Singhal-714@users.noreply.github.com>
2025-11-24 21:08:51 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1234,7 +1234,7 @@ function isSellerOnboardQuery(userMessage) {
 function sellerOnboardMessage() {
   const link = 'https://app.zulu.club/brand';
   const fillform = 'https://forms.gle/tvkaKncQMs29dPrPA';
-  return `Want to sell on Zulu Club? Sign up here: ${link}\\n\\nQuick steps:\\n• Fill the seller form at the link (${fillform})\\n• Our team will review & reach out\\n• Start listing products & reach Gurgaon customers`;
+  return "Want to sell on Zulu Club? Sign up here: https://app.zulu.club/brand\n\nQuick steps:\n• Fill the seller form at the link (https://forms.gle/tvkaKncQMs29dPrPA)\n• Our team will review & reach out\n• Start listing products & reach Gurgaon customers";
 }
 
 /* -------------------------
