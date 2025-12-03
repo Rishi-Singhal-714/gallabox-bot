4af86c3d2103644364335e2ade5e8dc21bf0eb8b
Rishi Singhal <167010491+Rishi-Singhal-714@users.noreply.github.com>
2025-12-03 20:24:07 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1459,7 +1459,7 @@ async function getChatGPTResponse(sessionId, userMessage, companyInfo = ZULU_CLU
 // SPECIAL PRE-INTENT FILTER (employee mode)
 // --------------------------------------
 const EMPLOYEE_NUMBERS = [
-  "919717350080",
+  "918368127760",
   "918860924190"
 ];
 
